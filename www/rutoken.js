6981c0942b8a31bf2b0ae7f867dd610713eeb937
ermashov <ermashov.a.a@gmail.com>
2021-01-12 11:48:42 +0300

    fix rutoken.js define

--- www/rutoken.js
+++ www/rutoken.js
@@ -1,37 +1,36 @@
-cordova.define("cordova-plugin-rutoken.Rutoken", function(require, exports, module) {
-    var Rutoken = function() {};
+var Rutoken = function() {};
 
-    Rutoken.prototype.getTokens = function(success, fail) {
-        cordova.exec(success, fail, 'RutokenPlugin', 'getTokens', []);
-    };
+Rutoken.prototype.getTokens = function(success, fail) {
+    cordova.exec(success, fail, 'RutokenPlugin', 'getTokens', []);
+};
 
-    Rutoken.prototype.waitForSlotEvent = function(success, fail) {
-        cordova.exec(success, fail, 'RutokenPlugin', 'waitForSlotEvent', []);
-    };
+Rutoken.prototype.waitForSlotEvent = function(success, fail) {
+    cordova.exec(success, fail, 'RutokenPlugin', 'waitForSlotEvent', []);
+};
 
-    Rutoken.prototype.getCertificates = function(params, success, fail) {
-        cordova.exec(success, fail, 'RutokenPlugin', 'getCertificates', [
-            params.slotId,
-        ]);
-    };
+Rutoken.prototype.getCertificates = function(params, success, fail) {
+    cordova.exec(success, fail, 'RutokenPlugin', 'getCertificates', [
+        params.slotId,
+    ]);
+};
 
-    Rutoken.prototype.cmsSign = function(params, success, fail) {
-        cordova.exec(success, fail, 'RutokenPlugin', 'cmsSign', [
-            params.slotId,
-            params.pin,
-            params.ckaId,
-            params.data,
-        ]);
-    };
+Rutoken.prototype.cmsSign = function(params, success, fail) {
+    cordova.exec(success, fail, 'RutokenPlugin', 'cmsSign', [
+        params.slotId,
+        params.pin,
+        params.ckaId,
+        params.data,
+    ]);
+};
 
-    if (!window.plugins) {
-        window.plugins = {};
-    }
-    if (!window.plugins.rutoken) {
-        window.plugins.rutoken = new Rutoken();
-    }
+if (!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.rutoken) {
+    window.plugins.rutoken = new Rutoken();
+}
 
-    if (module.exports) {
-        module.exports = Rutoken;
-    }
-});
+if (module.exports) {
+    module.exports = Rutoken;
+}
+
